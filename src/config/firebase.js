99815e5b577516a5dbad538775cb37dd96616d6f
Phanindra-Kodela <phanindra915@gmail.com>
2026-04-07 08:54:15 +0000

    chore: move Firebase keys to .env variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOURAPIKEY", // add your api key here
-  authDomain: "trinetra-game.firebaseapp.com",
-  projectId: "trinetra-game",
-  storageBucket: "trinetra-game.firebasestorage.app",
-  messagingSenderId: "125179785125",
-  appId: "1:125179785125:web:64f0eefe8b906a0146424c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
